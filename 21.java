45beca5916b0841b374528bf4bdaaca62a6de5b2
sanjay1151 <35390831+sanjay1151@users.noreply.github.com>
2018-03-06 09:38:37 +0530

    Update 21.java

--- 21.java
+++ 21.java
@@ -2,15 +2,12 @@ import java.util.*;
 import java.lang.*;
 public class arth{
       public static void main(String aa[]){
-      int n;
-      int f;
-      int l;
       int sum=0;
       int val;
       Scanner s=new Scanner(System.in);
-      n=s.nextInt();
-      f=s.nextInt();
-      l=s.nextInt();
+      int n=s.nextInt();
+      int f=s.nextInt();
+      int l=s.nextInt();
       val=f;
       for(int i=0;i<n;i++){
           sum=sum+val;
